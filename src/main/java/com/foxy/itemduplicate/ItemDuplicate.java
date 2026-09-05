66c236480588a1185fae5a90eee8e4aa42939eca
FoxyLeo <leitosotelo222@gmail.com>
2026-09-05 02:44:26 -0300

    Initial project setup

---
+++ src/main/java/com/foxy/itemduplicate/ItemDuplicate.java
@@ -0,0 +1,16 @@
+package com.foxy.itemduplicate;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ItemDuplicate extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
